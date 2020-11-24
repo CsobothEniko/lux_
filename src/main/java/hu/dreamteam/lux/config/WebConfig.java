fc7fb5536b441e9871c579fd1a29a7d52a51eb89
Rakonczai Adam <radamka97@gmail.com>
2020-11-24 17:40:20 +0100

    problem 4

--- src/main/java/hu/dreamteam/lux/config/WebConfig.java
+++ src/main/java/hu/dreamteam/lux/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("/auth/login");
+        registry.addViewController("/login").setViewName("auth/login");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 }
